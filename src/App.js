7cc1aeef8f3f09c546293f580b76a3237acefd7e
Drazen Simonovic <drazensimonovic18@gmail.com>
2023-06-02 13:54:34 +0200

    Tabs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
+  const [jobs, setJobs] = React.useState([]);
+  const [value, setValue] = React.useState(0);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, []);
+  console.log(jobs);
+  console.log(value);
+
+  const { company, dates, duties, title } = jobs[value] || {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="btn-container">
+        {jobs.map((item, index) => {
+          return (
+            <button
+              key={item.id}
+              onClick={() => setValue(index)}
+              className={`job-btn ${index === value && "active-btn"} `}
+            >
+              {item.company}
+            </button>
+          );
+        })}
+      </div>
+      <div className="App">
+        <h1 className="title">{title}</h1>
+        <h3 className="company">{company}</h3>
+        <h4 className="dates">{dates}</h4>
+        {duties &&
+          duties.map((duty, index) => (
+            <div key={index} className="job-desc">
+              <span class="material-symbols-outlined">
+                keyboard_double_arrow_right
+              </span>
+              <p className="duty">{duty}</p>
+            </div>
+          ))}
+      </div>
+    </section>
   );
 }
 
